11c97ae28dbdbeb97e696bd98eae51065de7c7a1
jkl5616 <jkl5616@gmail.com>
2013-02-14 00:06:11 +0900

    Revert "Update Welcome app"
    This reverts commit cc61b50035dcab42eec715f9bbabd0b91e99d1df.

--- src/com/thinkingbridge/welcome/BootCompleteReceiver.java
+++ src/com/thinkingbridge/welcome/BootCompleteReceiver.java
@@ -1,6 +1,20 @@
-package com.thinkingbridge.welcome;
+/*
+ * Copyright (C) 2012 Crossbones Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.lang.reflect.Method;
+package com.thinkingbridge.welcome;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -11,41 +25,32 @@ import android.util.Log;
 
 public class BootCompleteReceiver extends BroadcastReceiver {
 
-    private static final String CHANGELOG_INTENT = "tb.welcome.ChangeLogActivity";
-    private static final String WELCOME_INTENT = "tb.welcome.MainActivity";
+    private static final String WELCOME_INTENT = "com.thinkingbridge.welcome.AboutActivity";
     private static final String TAG = "WelcomeBootReceiver";
-    int show;
-    String a;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        MainActivity aboutActivity = new MainActivity();
-        Log.e(TAG, "Started");
+        AboutActivity aboutActivity = new AboutActivity();
 
-        SharedPreferences prefs = context.getSharedPreferences(MainActivity.PREFS_NAME, 0);
-        show = prefs.getInt(MainActivity.SHOW, 0);
-        String previousRomVersion = prefs.getString(MainActivity.ROM_VERSION, "0.0.0");
+        SharedPreferences prefs = context.getSharedPreferences(aboutActivity.PREFS_NAME, 0);
+
+        String previousRomVersion = prefs.getString(aboutActivity.ROM_VERSION, "0.0.0");
         String currentRomVersion = aboutActivity.getRomVersion();
-        Log.e(TAG, "Previous ROM Version: " + previousRomVersion);
-        Log.e(TAG, "Current ROM Version: " + currentRomVersion);
-        Log.e(TAG,"show");
-        if(show == 1){
-            if (!currentRomVersion.equals(previousRomVersion)) {
-                //if (currentRomVersion.equals(previousRomVersion)) { //DEBUGGING
-                    Log.e(TAG, "Running ChangeLog Activity");
 
-                    Intent i = new Intent();
-                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    i.setClassName(context, CHANGELOG_INTENT);
-                    context.startActivity(i);
-                } 
-        }
-        if(show == 0){
-            Log.e(TAG, "Running Welcome Activity");
+        Log.d(TAG, "Previous ROM Version: " + previousRomVersion);
+        Log.d(TAG, "Current ROM Version: " + currentRomVersion);
+
+        if (!currentRomVersion.equals(previousRomVersion)) {
+        //if (currentRomVersion.equals(previousRomVersion)) { //DEBUGGING
+            Log.d(TAG, "Running Welcome Activity");
+
             Intent i = new Intent();
             i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             i.setClassName(context, WELCOME_INTENT);
             context.startActivity(i);
+        } else {
+            Log.d(TAG, "Welcome Activity has already run");
         }
+
     }
 }
